93df4719b44755d2248e72f7e1ebc967c8147827
cristianbg2007 <cristianfelipebeltrangonzalez@gmail.com>
2026-05-14 11:54:32 -0500

    commit inicial plantilla

---
+++ gitFlow.py
@@ -0,0 +1,27 @@
+#calculadora con git flow
+
+import calculadora as calc
+    
+def calcsuma():
+    # aqui se va calcular la suma
+    return
+
+
+def calculadora_restando():
+    #se va calcular la resta
+    return
+
+
+def iniciar_aplicacion():
+    opcion=int(input("digite 1 sumar. digite 2 restar: "))
+    
+    if(opcion == 1):
+        n1=int(input("digite el primer numero"))
+        n2=int(input("digite el segundo numero"))
+
+        #mandar a llamar la funcion
+    else:
+        n1=int(input("digite el primer numero"))
+        n2=int(input("digite el segundo numero"))
+
+        #mandar a llamar la funcion
